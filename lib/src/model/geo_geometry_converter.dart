e476d66d028abcaeeef1dde316ae44361888536f
Roland Wimmer <roland.wimmer@grappfruit.com>
2023-09-18 16:02:59 +0200

    add instructions for next step

--- lib/src/model/geo_geometry_converter.dart
+++ lib/src/model/geo_geometry_converter.dart
@@ -23,12 +23,12 @@ class GeoGeometryConverter implements JsonConverter<GeoGeometry, dynamic> {
     final jsonShapes = json['coordinates'] as List<List<List<double>>>;
     print('>>> ${jsonShapes.length}: ${jsonShapes[0].length}');
 
-    final border = _mapJsonShape(jsonShapes[0], 0);
+    final border = _mapJsonShape(jsonShapes[0]);
 
     final holes = <GeoShape>[];
     if (json['coordinates'].length > 1) {
-      for (var i = 1; i < json['coordinates'].length; i++) {
-        final hole = _mapJsonShape(json, i);
+      for (var i = 1; i < jsonShapes.length; i++) {
+        final hole = _mapJsonShape(jsonShapes[i]);
         holes.add(hole);
       }
     }
@@ -39,8 +39,8 @@ class GeoGeometryConverter implements JsonConverter<GeoGeometry, dynamic> {
     );
   }
 
-  GeoShape _mapJsonShape(List<List<double>> jsonShape, int index) {
-    return GeoShape(coords: _coordsToGeoCoords(jsonShape));
+  GeoShape _mapJsonShape(List<List<double>> jsonShapes) {
+    return GeoShape(coords: _coordsToGeoCoords(jsonShapes));
   }
 
   List<GeoCoordinate> _coordsToGeoCoords(List<List<double>> borderCoordsJson) {
@@ -53,3 +53,22 @@ class GeoGeometryConverter implements JsonConverter<GeoGeometry, dynamic> {
   List<List<List<double>>> toJson(GeoGeometry object) =>
       throw UnimplementedError('toJson not implemented');
 }
+
+// chat gpt says:
+// import 'package:proj4dart/proj4dart.dart';
+//
+// void main() {
+//   // Define the UTM projection and WGS84 projection
+//   final utmProjection = Projection.add(Projection.getByName('EPSG:32633'));
+//   final wgs84Projection = Projection.add(Projection.getByName('EPSG:4326'));
+//
+//   // UTM coordinates (zone, easting, northing)
+//   final utmCoordinates = UTM.fromUTM(zone: 33, easting: 500000.0, northing: 4649776.0);
+//
+//   // Convert UTM to decimal degrees
+//   final wgs84Coordinates = utmProjection.transform(wgs84Projection, utmCoordinates);
+//
+//   // Print the result
+//   print('Latitude: ${wgs84Coordinates.lat}');
+//   print('Longitude: ${wgs84Coordinates.lon}');
+// }
